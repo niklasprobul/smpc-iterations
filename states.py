45911bb536c7a1a3dc0c6e2a8321ed5514efbfb2
niklas.probul <niklas.probul@tum.de>
2023-07-06 15:00:25 +0200

    added manual ack modes

--- states.py
+++ states.py
@@ -5,8 +5,8 @@ from FeatureCloud.app.engine.app import AppState, app_state, Role, SMPCOperation
 
 smpc = True
 sleep = False
+use_ack = False
 n_iterations = 100
-
 
 
 @app_state('initial', Role.BOTH)
@@ -14,7 +14,7 @@ class InitialState(AppState):
 
     def register(self):
         self.register_transition('local computation', Role.BOTH)
-        
+
     def run(self) -> str or None:
         self.store('iteration', 0)
         return 'local computation'
@@ -22,20 +22,22 @@ class InitialState(AppState):
 
 @app_state('local computation', Role.BOTH)
 class LocalComputationState(AppState):
-    
+
     def register(self):
         self.register_transition('global aggregation', Role.COORDINATOR)
         self.register_transition('wait for aggregation', Role.PARTICIPANT)
         self.register_transition('terminal', Role.BOTH)
-  
+
     def run(self) -> str or None:
         print('Local computation')
         iteration = self.load('iteration')
         print(f'iteration: {iteration}')
-        
+
         self.store('iteration', iteration + 1)
 
-        if len(self._app.data_outgoing) > 0:
+        if len(self._app.data_outgoing) > 0 and False:
+            #print(f'last outgoing data: {self._app.data_outgoing[0]}')
+            # outgoing = pickle.loads(self._app.data_outgoing[0][0])
             outgoing = pickle.loads(self._app.data_outgoing[0][0])
             print(f'data_outgoing: {outgoing}')
             print('####\nData outgoing not empty\n####')
@@ -43,8 +45,8 @@ class LocalComputationState(AppState):
 
         # returns an array with len = number of clients, 1 for the client id of the executor, 0 otherwise
         # should result in a n array with all 1s after aggregation
-        data_to_send = [1 if self.id==_ else 0 for _ in self.clients]
-        
+        #data_to_send = [1 if self.id == _ else 0 for _ in self.clients]
+        data_to_send = 1
         if smpc:
             self.configure_smpc(operation=SMPCOperation.ADD)
         self.send_data_to_coordinator(data_to_send, use_smpc=smpc)
@@ -54,36 +56,37 @@ class LocalComputationState(AppState):
             return 'global aggregation'
         else:
             return 'wait for aggregation'
-                
+
 
 @app_state('wait for aggregation', Role.PARTICIPANT)
 class WaitForAggregationState(AppState):
-    
+
     def register(self):
         self.register_transition('local computation', Role.PARTICIPANT)
         self.register_transition('terminal', Role.PARTICIPANT)
-     
+
     def run(self) -> str or None:
         print('Wait for aggregation')
         data = self.await_data()
         print(f'Received aggregated data: {data}')
-        
+
         if data != [1 for _ in self.clients]:
             print('####\nData not aggregated correctly\n####')
             return 'terminal'
-        
-        self.send_data_to_coordinator(0, use_smpc=False)
-        print('Send okay to the coordinator')
-        
-        okay = self.await_data()
-        print(f'Received okay from coordinator: {okay}')
+
+        if use_ack:
+            self.send_data_to_coordinator(0, use_smpc=False)
+            print('Send okay to the coordinator')
+
+            okay = self.await_data()
+            print(f'Received okay from coordinator: {okay}')
 
         if self.load('iteration') < n_iterations:
             return 'local computation'
         else:
             return 'terminal'
-   
-   
+
+
 @app_state('global aggregation', Role.COORDINATOR)
 class GlobalAggregationState(AppState):
 
@@ -93,25 +96,26 @@ class GlobalAggregationState(AppState):
 
     def run(self) -> str or None:
         if smpc:
+            print(f'aggregating data')
             data = self.aggregate_data(SMPCOperation.ADD, use_smpc=smpc)
             #data = self.await_data(1, unwrap=True, is_json=True)
         else:
             data = self.gather_data()
         print(f'Received data of all clients: {data}')
-        
-        
-        
-        self.broadcast_data(data, send_to_self=False)
+
+    
+        self.broadcast_data(data, send_to_self=True)
         print('Broadcasting computation data to clients')
         
-        self.send_data_to_coordinator(0, use_smpc=False)
-        print('Coordinator sends okay')
-        
-        okay = self.gather_data()
-        print(f'Receive okay from all clients: {okay}')
+        if use_ack:
+            self.send_data_to_coordinator(0, use_smpc=False)
+            print('Coordinator sends okay')
 
-        self.broadcast_data(0, send_to_self=False)
-        print('Send okay to all clients')
+            okay = self.gather_data()
+            print(f'Receive okay from all clients: {okay}')
+
+            self.broadcast_data(0, send_to_self=False)
+            print('Send okay to all clients')
 
         if sleep:
             time.sleep(100)
